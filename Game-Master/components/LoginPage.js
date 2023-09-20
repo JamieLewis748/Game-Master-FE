2f20e87c977ee1db231871a3423566168a70755d
IanTibble1 <iantibble1@gmail.com>
2023-09-20 11:19:59 +0100

    improves styling of login page wireframe

--- Game-Master/components/LoginPage.js
+++ Game-Master/components/LoginPage.js
@@ -7,6 +7,7 @@ import {
   StyleSheet,
   View,
   Image,
+  Text,
 } from "react-native";
 
 function LoginPage({ navigation }) {
@@ -32,57 +33,88 @@ function LoginPage({ navigation }) {
   };
 
   return (
-    <SafeAreaView
-      style={{ flex: 1, justifyContent: "space-evenly", padding: 16 }}
-    >
-      <View style={styles.imageContainer}>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.logoContainer}>
         <Image
           style={styles.logo}
           source="https://www.designevo.com/res/templates/thumb_small/banner-board-jungle-logo.webp"
         />
       </View>
-      <View>
+      <View style={styles.card}>
+        <Text style={{ fontSize: 20, alignSelf: "center", marginBottom: 5 }}>
+          Login
+        </Text>
         <TextInput
-          style={{ borderWidth: 1, padding: 10, marginBottom: 10 }}
+          style={styles.inputbar}
           placeholder="Username"
           value={username}
           onChangeText={setUsername}
         />
         <TextInput
-          style={{ borderWidth: 1, padding: 10, marginBottom: 10 }}
+          style={styles.inputbar}
           placeholder="Password"
           value={password}
           onChangeText={setPassword}
           secureTextEntry
         />
-        <View style={styles.button}>
+        <View style={styles.buttonContainer}>
           <Button title="Login" onPress={handleLogin} />
         </View>
-        <View>
+        <View style={styles.buttonContainer}>
           <Button title="Create Account" onPress={handleSignUp} />
         </View>
-        <View style={styles.button}>
-          <Button title="See events" onPress={handleEventLog} />
-        </View>
-        <View style={styles.button}>
-          <Button title="Create Event" onPress={handleCreateEvent} />
-        </View>
+      </View>
+      <View style={styles.buttonContainer}>
+        <Button title="See events" onPress={handleEventLog} />
+      </View>
+      <View style={styles.buttonContainer}>
+        <Button title="Create Event" onPress={handleCreateEvent} />
       </View>
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  button: {
-    marginBottom: 20,
-  },
-  logo: {
-    height: 250,
-    width: 250,
-  },
-  imageContainer: {
+  container: {
+    flex: 1,
     justifyContent: "center",
     alignItems: "center",
+    padding: 20,
+  },
+  card: {
+    width: "90%",
+    padding: 20,
+    borderRadius: 10,
+    backgroundColor: "white",
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+  },
+  inputbar: {
+    height: 40,
+    width: "100%",
+    borderColor: "gray",
+    borderWidth: 1,
+    marginTop: 10,
+    paddingHorizontal: 10,
+  },
+
+  logoContainer: {
+    marginBottom: 10,
+  },
+
+  logo: {
+    width: 200,
+    height: 200,
+    borderRadius: 50,
+  },
+
+  buttonContainer: {
+    marginTop: 10,
+    width: 150,
+    alignSelf: "center",
   },
 });
 
