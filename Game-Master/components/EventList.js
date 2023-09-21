477e63fb88c8348927bd0e5b1eb14fa4193f4de6
IanTibble1 <iantibble1@gmail.com>
2023-09-21 10:57:30 +0100

    adds expandable with button working

--- Game-Master/components/EventList.js
+++ Game-Master/components/EventList.js
@@ -1,15 +1,53 @@
 import React, { useState, useEffect } from "react";
-import { FlatList, Image, StyleSheet, View, Animated } from "react-native";
+import {
+  FlatList,
+  Image,
+  StyleSheet,
+  View,
+  Animated,
+  TouchableWithoutFeedback,
+  Button,
+} from "react-native";
 import { Card, Paragraph, IconButton } from "react-native-paper";
 import axios from "axios";
-import { TouchableWithoutFeedback } from "react-native-web";
-const axiosBase = axios.create({
-  baseURL: "https://game-master-be.onrender.com/api/",
-});
 
-import EventData from "../assets/data/event.data";
+// const axiosBase = axios.create({
+//   baseURL: "https://game-master-be.onrender.com/api/",
+// });
 
-const fetchEvents = () => axiosBase.get("events");
+// const fetchEvents = () => axiosBase.get("events");
+
+const DATA = [
+  {
+    event_id: "1",
+    host_id: "1",
+    image: "https://media.timeout.com/images/105627949/750/422/image.jpg",
+    public: true,
+    game_type: "boardgame",
+    location: "locationdata",
+    time: 10,
+    date: "01/01/01",
+    capacity: "full",
+    current_attending: ["user1", "user2", "user3"],
+    prize: `https://img.itch.zone/aW1nLzEzMzIzNDA0LmdpZg==/original/QUoq1B.gif`,
+    description:
+      "Looking for fellow Monopoly lovers for a weekly session. All experiences levels welcome ",
+  },
+
+  {
+    event_id: "2",
+    host_id: "2",
+    public: false,
+    game_type: "cardgame",
+    location: "locationdata",
+    time: 11,
+    date: "02/01/01",
+    capacity: "3/5",
+    prize: `https://img.itch.zone/aW1nLzEzMzIzMzkzLmdpZg==/original/A7JFLC.gif`,
+    description:
+      "Looking for fellow Monopoly lovers for a weekly session. All experiences levels welcome ",
+  },
+];
 
 const EventList = ({ navigation }) => {
   const [currentEventList, setCurrentEventList] = useState([]);
@@ -24,13 +62,33 @@ const EventList = ({ navigation }) => {
   //     });
   // }, []);
 
+  const handleMouseEnter = () => {
+    Animated.spring(scale, {
+      toValue: 1.05,
+      friction: 3,
+      useNativeDriver: false,
+    }).start();
+  };
+
+  const handleMouseLeave = () => {
+    Animated.spring(scale, {
+      toValue: 1,
+      friction: 3,
+      useNativeDriver: false,
+    }).start();
+  };
+
   const EventItem = ({ event }) => {
     const [isExpanded, setIsExpanded] = useState(false);
     const scale = new Animated.Value(1);
 
     return (
       <TouchableWithoutFeedback onPress={() => setIsExpanded(!isExpanded)}>
-        <Animated.View style={{ ...styles.container, transform: [{ scale }] }}>
+        <Animated.View
+          style={{ ...styles.container, transform: [{ scale }] }}
+          onMouseEnter={handleMouseEnter}
+          onMouseLeave={handleMouseLeave}
+        >
           <Card style={styles.container}>
             <View style={styles.eventInfoContainer}>
               <Image source={{ uri: event.image }} style={styles.eventImage} />
@@ -74,7 +132,7 @@ const EventList = ({ navigation }) => {
   return (
     <View style={{ flex: 1 }}>
       <FlatList
-        data={EventData}
+        data={DATA}
         renderItem={renderItem}
         keyExtractor={(item) => item._id}
         numColumns={1}
