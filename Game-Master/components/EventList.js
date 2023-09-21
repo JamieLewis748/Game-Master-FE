d8d2b6122f1ffdc18e5ad0a1969e9e2807ed122f
IanTibble1 <iantibble1@gmail.com>
2023-09-21 10:42:36 +0100

    adds expandable button

--- Game-Master/components/EventList.js
+++ Game-Master/components/EventList.js
@@ -2,62 +2,70 @@ import React, { useState, useEffect } from "react";
 import { FlatList, Image, StyleSheet, View, Animated } from "react-native";
 import { Card, Paragraph, IconButton } from "react-native-paper";
 import axios from "axios";
-
+import { TouchableWithoutFeedback } from "react-native-web";
 const axiosBase = axios.create({
   baseURL: "https://game-master-be.onrender.com/api/",
 });
+
+import EventData from "../assets/data/event.data";
 
 const fetchEvents = () => axiosBase.get("events");
 
 const EventList = ({ navigation }) => {
   const [currentEventList, setCurrentEventList] = useState([]);
 
-  useEffect(() => {
-    fetchEvents()
-      .then(({ data }) => {
-        setCurrentEventList(data.events);
-      })
-      .catch((err) => {
-        console.error("Error fetching events: ", err);
-      });
-  }, []);
+  // useEffect(() => {
+  //   fetchEvents()
+  //     .then(({ data }) => {
+  //       setCurrentEventList(data.events);
+  //     })
+  //     .catch((err) => {
+  //       console.error("Error fetching events: ", err);
+  //     });
+  // }, []);
 
   const EventItem = ({ event }) => {
+    const [isExpanded, setIsExpanded] = useState(false);
     const scale = new Animated.Value(1);
 
     return (
-      <Animated.View style={{ ...styles.container, transform: [{ scale }] }}>
-        <Card style={styles.container}>
-          <View style={styles.eventInfoContainer}>
-            <Image source={{ uri: event.image }} style={styles.eventImage} />
-            <View style={styles.infoContainer}>
-              <Paragraph>
-                <IconButton icon="calendar" size={16} color="gray" />
-                {event.date}
-                <IconButton icon="clock-outline" size={16} color="gray" />
-                {event.time}
-              </Paragraph>
-              <Paragraph>
-                <IconButton icon="account-group" size={16} color="gray" />
-                {event.capacity}
-                <IconButton icon="map-marker" size={16} color="gray" />
-                {event.location}
-              </Paragraph>
+      <TouchableWithoutFeedback onPress={() => setIsExpanded(!isExpanded)}>
+        <Animated.View style={{ ...styles.container, transform: [{ scale }] }}>
+          <Card style={styles.container}>
+            <View style={styles.eventInfoContainer}>
+              <Image source={{ uri: event.image }} style={styles.eventImage} />
+              <View style={styles.infoContainer}>
+                <Paragraph>
+                  <IconButton icon="calendar" size={16} color="gray" />
+                  {event.date}
+                  <IconButton icon="clock-outline" size={16} color="gray" />
+                  {event.time}
+                </Paragraph>
+                <Paragraph>
+                  <IconButton icon="account-group" size={16} color="gray" />
+                  {event.capacity}
+                  <IconButton icon="map-marker" size={16} color="gray" />
+                  {event.location}
+                </Paragraph>
+              </View>
             </View>
-          </View>
-          <Card.Actions>
-            {/* <Button
-            mode="contained"
-            colour="purple"
-            onPress={() =>
-              navigation.navigate("Event Details", { selectedEvent: event })
-            }
-          >
-            See event
-          </Button> */}
-          </Card.Actions>
-        </Card>
-      </Animated.View>
+            <Card.Actions>
+              {isExpanded && (
+                <Button
+                  title="See event"
+                  mode="contained"
+                  colour="purple"
+                  onPress={() =>
+                    navigation.navigate("Event Details", {
+                      selectedEvent: event,
+                    })
+                  }
+                />
+              )}
+            </Card.Actions>
+          </Card>
+        </Animated.View>
+      </TouchableWithoutFeedback>
     );
   };
 
@@ -66,7 +74,7 @@ const EventList = ({ navigation }) => {
   return (
     <View style={{ flex: 1 }}>
       <FlatList
-        data={currentEventList}
+        data={EventData}
         renderItem={renderItem}
         keyExtractor={(item) => item._id}
         numColumns={1}
