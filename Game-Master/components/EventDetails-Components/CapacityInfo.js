3cf7095b0f22ddbaf52109af5adeef1bedd934c4
IanTibble1 <iantibble1@gmail.com>
2023-09-22 13:43:26 +0100

    adds formatting to event details

--- Game-Master/components/EventDetails-Components/CapacityInfo.js
+++ Game-Master/components/EventDetails-Components/CapacityInfo.js
@@ -2,13 +2,14 @@ import react from "react";
 import { Paragraph, IconButton } from "react-native-paper";
 import { View } from "react-native";
 
-const CapacityInfo = ({ capacity }) => {
+const CapacityInfo = ({ capacity, participants }) => {
   return (
     <View style={{ flexDirection: "row", alignItems: "center" }}>
       <IconButton icon="account-group" size={16} color="gray" />
       <Paragraph>
         <Paragraph style={{ fontWeight: "bold" }}>Capacity: </Paragraph>
-        {capacity}
+
+        {`${participants} / ${capacity}`}
       </Paragraph>
     </View>
   );
