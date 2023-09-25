3fc67516c57c49b7b325942dd067397f20c4be70
liamjones2 <liammjoness@gmail.com>
2023-09-25 09:43:59 +0100

    removed console logs

--- Game-Master/components/APIs/postUser.js
+++ Game-Master/components/APIs/postUser.js
@@ -4,7 +4,6 @@ import { auth } from "../Authentication/firebase-config";
 
 
 const PostNewUser = async (name, username, email, img_url, characterName, password, setDbUser) => {
-    console.log(name, username, email, img_url, characterName)
     return axios
         .post(`https://game-master-be.onrender.com/api/users`, {
             name: name,
@@ -14,20 +13,16 @@ const PostNewUser = async (name, username, email, img_url, characterName, passwo
             characterName: characterName,
         })
         .then(async ({ data }) => {
-            console.log(data)
             if (data.acknowledged === true) {
                 await createUserWithEmailAndPassword(auth, email, password);
-                const user = await axios.get(`https://game-master-be.onrender.com/api/users/${data.insertedId}`,
-                    { userWhoRequested: "00000020f51bb4362eee2a02" }
-                )
+                const user = await axios.get(`https://game-master-be.onrender.com/api/users/${data.insertedId}`)
                 setDbUser(user.user)
-                return;
+                return { acknowledged: true }
             } else {
-                return;
+                return { acknowledged: false }
             }
-
-        }).then(() => {
-            return { acknowledged: true }
+        }).then((response) => {
+            return response
         })
 };
 
