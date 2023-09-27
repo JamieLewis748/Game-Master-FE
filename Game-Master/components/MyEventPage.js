65d215a91a8e2572bf67847a18000ef328a9424d
liamjones2 <liammjoness@gmail.com>
2023-09-27 15:49:28 +0100

    fixed errors

--- Game-Master/components/MyEventPage.js
+++ Game-Master/components/MyEventPage.js
@@ -1,23 +1,23 @@
 import {
-    SafeAreaView,
-    FlatList,
-    Image,
-    StyleSheet,
-    View,
-    ScrollView,
-    Pressable,
-    Modal,
+  SafeAreaView,
+  FlatList,
+  Image,
+  StyleSheet,
+  View,
+  ScrollView,
+  Pressable,
+  Modal,
 } from "react-native";
 import React, { useState, useEffect, useContext } from "react";
 
 import {
-    Text,
-    Button,
-    Card,
-    Paragraph,
-    Title,
-    Avatar,
-    IconButton,
+  Text,
+  Button,
+  Card,
+  Paragraph,
+  Title,
+  Avatar,
+  IconButton,
 } from "react-native-paper";
 import DateInfo from "./EventDetails-Components/DateInfo";
 import CapacityInfo from "./EventDetails-Components/CapacityInfo";
@@ -33,54 +33,54 @@ import { Picker } from "@react-native-picker/picker";
 
 
 const axiosBase = axios.create({
-    baseURL: "https://game-master-be.onrender.com/api/",
+  baseURL: "https://game-master-be.onrender.com/api/",
 });
 
 const fetchUsers = () => axiosBase.get("users");
 
 
 const MyEventPage = ({ route }) => {
-    const { selectedEvent } = route.params;
-    const [userList, setUserList] = useState([]);
-    const [selectedWinner, setSelectedWinner] = useState("");
-    const [showWarningModal, setShowWarningModal] = useState(false);
-    const [eventParticipants, setEventParticipants] = useState(selectedEvent.participants);
-    const [requestedParticipants, setRequestedParticipants] = useState(selectedEvent.requestedToParticipate);
+  const { selectedEvent } = route.params;
+  const [userList, setUserList] = useState([]);
+  const [selectedWinner, setSelectedWinner] = useState("");
+  const [showWarningModal, setShowWarningModal] = useState(false);
+  const [eventParticipants, setEventParticipants] = useState(selectedEvent.participants);
+  const [requestedParticipants, setRequestedParticipants] = useState(selectedEvent.requestedToParticipate);
 
-    const handleCompleteEvent = () => {
-        console.log("here")
-        if (!selectedWinner) {
-            setShowWarningModal(true);
-        } else {
-            console.log(selectedWinner)
-            completeEvent(selectedEvent._id, selectedEvent.hostedBy, eventParticipants, selectedWinner);
-        }
-    };
-
-    const updateParticipants = (user_id) => {
-        setEventParticipants((prevParticipants) => [...prevParticipants, user_id]);
-
-        setRequestedParticipants((prevParticipants) =>
-            prevParticipants.filter((id) => id !== user_id)
-        );
-    };
-
-    function getUsernameFromId(userId) {
-        const user = userList.find((user) => user._id === userId);
-        return user ? user.username : 'Unknown User';
+  const handleCompleteEvent = () => {
+    console.log("here")
+    if (!selectedWinner) {
+      setShowWarningModal(true);
+    } else {
+      console.log(selectedWinner)
+      completeEvent(selectedEvent._id, selectedEvent.hostedBy, eventParticipants, selectedWinner);
     }
+  };
 
-    useEffect(() => {
-        fetchUsers()
-            .then(({ data }) => {
-                setUserList(data);
-            })
-            .catch((err) => {
-                console.error("Error fetching events: ", err);
-            });
-    }, []);
+  const updateParticipants = (user_id) => {
+    setEventParticipants((prevParticipants) => [...prevParticipants, user_id]);
 
-    return (
+    setRequestedParticipants((prevParticipants) =>
+      prevParticipants.filter((id) => id !== user_id)
+    );
+  };
+
+  function getUsernameFromId(userId) {
+    const user = userList.find((user) => user._id === userId);
+    return user ? user.username : 'Unknown User';
+  }
+
+  useEffect(() => {
+    fetchUsers()
+      .then(({ data }) => {
+        setUserList(data);
+      })
+      .catch((err) => {
+        console.error("Error fetching events: ", err);
+      });
+  }, []);
+
+  return (
       <ScrollView>
         <SafeAreaView
           style={{
@@ -132,28 +132,31 @@ const MyEventPage = ({ route }) => {
                 )}
               </View>
               <View style={styles.attendeeList}>
-                {userList.length > 0 && (
-                  <Picker
-                    selectedValue={selectedWinner}
-                    onValueChange={(itemValue, itemIndex) => {
-                      const selectedWinnerIndex = parseInt(itemValue);
-                      const selectedWinner =
-                        selectedEvent.participants[selectedWinnerIndex];
-                      console.log("Selected Winner:", selectedWinner);
-                      setSelectedWinner(selectedWinner);
-                    }}
-                  >
-                    <Picker.Item label="Select a Winner" value={null} />
-                    {selectedEvent.participants.map((participant, index) => (
-                      <Picker.Item
-                        key={index.toString()}
-                        label={getUsernameFromId(participant)}
-                        value={participant}
-                      />
-                    ))}
-                  </Picker>
-                )}
-              </View>
+                            {userList.length > 0 && (
+
+                                < Picker
+                                    selectedValue={selectedWinner}
+                                    onValueChange={(itemValue, itemIndex) => {
+                                        console.log(itemValue)
+                                        const selectedWinnerIndex = eventParticipants.indexOf(itemValue);
+                                        console.log(selectedWinnerIndex)
+                                        const selectedWinner = eventParticipants[selectedWinnerIndex];
+                                        console.log('Selected Winner:', selectedWinner);
+                                        setSelectedWinner(selectedWinner);
+
+                                    }}
+                                >
+                                    <Picker.Item label="Select a Winner" value={null} />
+                                    {eventParticipants.map((participant, index) => (
+                                        <Picker.Item
+                                            key={index.toString()}
+                                            label={getUsernameFromId(participant)}
+                                            value={participant}
+                                        />
+                                    ))}
+                                </Picker>
+                            )}
+                        </View>
               <View styles={styles.attendeeList}>
                 {selectedEvent.isCompleted === "false" ? (
                   <>
@@ -229,94 +232,6 @@ const MyEventPage = ({ route }) => {
           </Modal>
         </SafeAreaView>
       </ScrollView>
-                >
-                    <Card.Content style={styles.eventCard}>
-                        <Card.Cover source={{ uri: selectedEvent.image }} />
-                        <View>
-                            <DescriptionInfo gameInfo={selectedEvent.gameInfo} />
-                        </View>
-                        <View style={styles.eventView}>
-                            <TimeInfo time={selectedEvent.dateTime} />
-                            <DateInfo date={selectedEvent.dateTime} />
-                        </View>
-                        <View style={styles.eventView}>
-                            <CapacityInfo
-                                capacity={selectedEvent.capacity}
-                                participants={selectedEvent.participants.length}
-                            />
-                            <PublicInfo public={selectedEvent.public} />
-                        </View>
-                        <View styles={styles.attendeeList}>
-                            {userList.length > 0 && (
-                                <AttendeesInfo
-                                    userList={userList}
-                                    host={selectedEvent.hostedBy}
-                                    participants={eventParticipants}
-                                />
-                            )}
-                        </View>
-                        <View styles={styles.attendeeList}>
-                            {userList.length > 0 && (
-                                <RequestedParticipantInfo
-                                    userList={userList}
-                                    requestedToParticipate={requestedParticipants}
-                                    event_id={selectedEvent._id}
-                                    setEventParticipants={setEventParticipants}
-                                    setRequestedParticipants={setRequestedParticipants}
-                                    onUpdateParticipants={updateParticipants}
-                                />
-                            )}
-                        </View>
-                        <View style={styles.attendeeList}>
-                            {userList.length > 0 && (
-
-                                < Picker
-                                    selectedValue={selectedWinner}
-                                    onValueChange={(itemValue, itemIndex) => {
-                                        console.log(itemValue)
-                                        const selectedWinnerIndex = eventParticipants.indexOf(itemValue);
-                                        console.log(selectedWinnerIndex)
-                                        const selectedWinner = eventParticipants[selectedWinnerIndex];
-                                        console.log('Selected Winner:', selectedWinner);
-                                        setSelectedWinner(selectedWinner);
-
-                                    }}
-                                >
-                                    <Picker.Item label="Select a Winner" value={null} />
-                                    {eventParticipants.map((participant, index) => (
-                                        <Picker.Item
-                                            key={index.toString()}
-                                            label={getUsernameFromId(participant)}
-                                            value={participant}
-                                        />
-                                    ))}
-                                </Picker>
-                            )}
-                        </View>
-                        <View styles={styles.attendeeList}>
-                            {selectedEvent.isCompleted === "false" ? (
-                                <Pressable onPress={handleCompleteEvent}><Text>Complete Event</Text></Pressable>
-                            ) : (
-                                <Text></Text>
-                            )}
-                        </View>;
-                    </Card.Content>
-                    <View >
-                        <Text>Event Prize:</Text>
-                        <MonsterImageSelection collectionId={selectedEvent.prizeCollection_id} />
-                    </View>
-                </Card>
-                <Modal
-                    visible={showWarningModal}
-                    onRequestClose={() => setShowWarningModal(false)}
-                >
-                    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-                        <Text>Please select a winner before completing the event.</Text>
-                        <Button onPress={() => setShowWarningModal(false)}>OK</Button>
-                    </View>
-                </Modal>
-            </SafeAreaView>
-        </ScrollView >
     );
 };
 
