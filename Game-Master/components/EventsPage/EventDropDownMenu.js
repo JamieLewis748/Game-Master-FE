bb3954f819226b5242c7a9322324de2f0df30d8d
BytingMicrochips <marcus.bishop.1995@live.co.uk>
2023-09-27 21:56:17 +0100

    made event queries bar fill page

--- Game-Master/components/EventsPage/EventDropDownMenu.js
+++ Game-Master/components/EventsPage/EventDropDownMenu.js
@@ -1,35 +1,38 @@
 import React, { useState } from "react";
 import { Text, Picker, View, StyleSheet } from "react-native";
+import { Card } from "react-native-paper";
 
 const DropDownMenu = ({ selectedValue, onValueChange, selectedTimeDateValue, onTimeDateValueChange }) => {
 
   return (
     <>
-      <View style={styles.dropdowns}>
-        <Text style={styles.pickers}>Date</Text>
-        <Picker
-          style={styles.pickers}
-          selectedValue={selectedTimeDateValue}
-          onValueChange={(itemValue) => onTimeDateValueChange(itemValue)}
-        >
-          <Picker.Item label="Next" />
-          <Picker.Item label="Last" />
-        </Picker>
+      <Card style={{ margin: 10}}>
+        <View style={styles.dropdowns}>
+          <Text style={styles.pickers}>Date</Text>
+          <Picker
+            style={styles.pickers}
+            selectedValue={selectedTimeDateValue}
+            onValueChange={(itemValue) => onTimeDateValueChange(itemValue)}
+          >
+            <Picker.Item label="Next" />
+            <Picker.Item label="Last" />
+          </Picker>
 
-        <Text style={styles.pickers}>Game type</Text>
-        <Picker
-          style={styles.pickers}
-          selectedValue={selectedValue}
-          onValueChange={(itemValue) => onValueChange(itemValue)}
-        >
-          <Picker.Item label="All" />
-          <Picker.Item label="Board Game" />
-          <Picker.Item label="Card Game" />
-          <Picker.Item label="Tabletop" />
-          <Picker.Item label="RPG" />
-          <Picker.Item label="Event happening" />
-        </Picker>
-      </View>
+          <Text style={styles.pickers}>Game type</Text>
+          <Picker
+            style={styles.pickers}
+            selectedValue={selectedValue}
+            onValueChange={(itemValue) => onValueChange(itemValue)}
+          >
+            <Picker.Item label="All" />
+            <Picker.Item label="Board Game" />
+            <Picker.Item label="Card Game" />
+            <Picker.Item label="Tabletop" />
+            <Picker.Item label="RPG" />
+            <Picker.Item label="Event happening" />
+          </Picker>
+        </View>
+      </Card>
     </>
   );
 }
@@ -38,14 +41,18 @@ const styles = StyleSheet.create({
   dropdowns: {
     display: "flex",
     flexDirection: "row",
-    width: "auto",
+    justifyContent: "space-between",
+    marginLeft: 15,
+    marginRight: 15,
   },
   pickers: {
     display: "flex",
     padding: 8,
     backgroundColor: 'rgb(37, 35, 42)',
     color: 'white',
-    textAlign: "left"
+    textAlign: "left",
+    borderColor: "purple",
+    borderRadius: 5
   },
 });
 
