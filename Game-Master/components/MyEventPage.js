ac3b7459cdbf224e3bb7d36f84587f5b3abc6f04
BytingMicrochips <marcus.bishop.1995@live.co.uk>
2023-09-27 15:26:28 +0100

    style buttons MyEventPage handleCompleteEvent

--- Game-Master/components/MyEventPage.js
+++ Game-Master/components/MyEventPage.js
@@ -51,6 +51,7 @@ const MyEventPage = ({ route }) => {
         if (!selectedWinner) {
             setShowWarningModal(true);
         } else {
+            setShowWarningModal(false);
             completeEvent(selectedEvent._id, selectedEvent.hostedBy, selectedEvent.participants, selectedWinner._id);
         }
     };
@@ -79,114 +80,172 @@ const MyEventPage = ({ route }) => {
     }, []);
 
     return (
-        <ScrollView>
-            <SafeAreaView
-                style={{
-                    backgroundColor: "purple",
-                    height: "100%",
-                    overflow: "hidden",
-                }}
-            >
-                <Card
-                    style={{
-                        margin: "auto",
-                        height: "97%",
-                        marginTop: 10,
+      <ScrollView>
+        <SafeAreaView
+          style={{
+            backgroundColor: "purple",
+            height: "100%",
+            overflow: "hidden",
+          }}
+        >
+          <Card
+            style={{
+              margin: "auto",
+              height: "97%",
+              marginTop: 10,
+            }}
+          >
+            <Card.Content style={styles.eventCard}>
+              <Card.Cover source={{ uri: selectedEvent.image }} />
+              <View>
+                <DescriptionInfo gameInfo={selectedEvent.gameInfo} />
+              </View>
+              <View style={styles.eventView}>
+                <TimeInfo time={selectedEvent.dateTime} />
+                <DateInfo date={selectedEvent.dateTime} />
+              </View>
+              <View style={styles.eventView}>
+                <CapacityInfo
+                  capacity={selectedEvent.capacity}
+                  participants={selectedEvent.participants.length}
+                />
+                <PublicInfo public={selectedEvent.public} />
+              </View>
+              <View styles={styles.attendeeList}>
+                {userList.length > 0 && (
+                  <AttendeesInfo
+                    userList={userList}
+                    host={selectedEvent.hostedBy}
+                    participants={eventParticipants}
+                  />
+                )}
+              </View>
+              <View styles={styles.attendeeList}>
+                {userList.length > 0 && (
+                  <RequestedParticipantInfo
+                    userList={userList}
+                    requestedToParticipate={requestedParticipants}
+                    event_id={selectedEvent._id}
+                    onUpdateParticipants={updateParticipants}
+                  />
+                )}
+              </View>
+              <View style={styles.attendeeList}>
+                {console.log("🚀 ~ participants:", selectedEvent.participants)}
+                {userList.length > 0 && (
+                  <Picker
+                    selectedValue={selectedWinner}
+                    onValueChange={(itemValue, itemIndex) => {
+                      const selectedWinnerIndex = parseInt(itemValue);
+                      const selectedWinner =
+                        selectedEvent.participants[selectedWinnerIndex];
+                      console.log("Selected Winner:", selectedWinner);
+                      setSelectedWinner(selectedWinner);
                     }}
-                >
-                    <Card.Content style={styles.eventCard}>
-                        <Card.Cover source={{ uri: selectedEvent.image }} />
-                        <View>
-                            <DescriptionInfo gameInfo={selectedEvent.gameInfo} />
-                        </View>
-                        <View style={styles.eventView}>
-                            <TimeInfo time={selectedEvent.dateTime} />
-                            <DateInfo date={selectedEvent.dateTime} />
-                        </View>
-                        <View style={styles.eventView}>
-                            <CapacityInfo
-                                capacity={selectedEvent.capacity}
-                                participants={selectedEvent.participants.length}
-                            />
-                            <PublicInfo public={selectedEvent.public} />
-                        </View>
-                        <View styles={styles.attendeeList}>
-                            {userList.length > 0 && (
-                                <AttendeesInfo
-                                    userList={userList}
-                                    host={selectedEvent.hostedBy}
-                                    participants={eventParticipants}
-                                />
-                            )}
-                        </View>
-                        <View styles={styles.attendeeList}>
-                            {userList.length > 0 && (
-                                <RequestedParticipantInfo
-                                    userList={userList}
-                                    requestedToParticipate={requestedParticipants}
-                                    event_id={selectedEvent._id}
-                                    onUpdateParticipants={updateParticipants}
-                                />
-                            )}
-                        </View>
-                        <View style={styles.attendeeList}>
-                            {console.log("🚀 ~ participants:", selectedEvent.participants)}
-                            {userList.length > 0 && (
-
-                                < Picker
-                                    selectedValue={selectedWinner}
-                                    onValueChange={(itemValue, itemIndex) => {
-                                        const selectedWinnerIndex = parseInt(itemValue);
-                                        const selectedWinner = selectedEvent.participants[selectedWinnerIndex];
-                                        console.log('Selected Winner:', selectedWinner);
-                                        setSelectedWinner(selectedWinner);
-
-                                    }}
-                                >
-                                    <Picker.Item label="Select a Winner" value={null} />
-                                    {selectedEvent.participants.map((participant, index) => (
-                                        <Picker.Item
-                                            key={index.toString()}
-                                            label={getUsernameFromId(participant)}
-                                            value={participant}
-                                        />
-                                    ))}
-                                </Picker>
-                            )}
-                        </View>
-                        <View styles={styles.attendeeList}>
-                            {selectedEvent.isCompleted === "false" ? (
-                                <Pressable onPress={() => handleCompleteEvent}><Text>Complete Event</Text></Pressable>
-                            ) : (
-                                <Text>This event is already completed</Text>
-                            )}
-                        </View>;
-                    </Card.Content>
-                    <View >
-                        <Text>Event Prize:</Text>
-                        <MonsterImageSelection collectionId={selectedEvent.prizeCollection_id} />
-                    </View>
-                </Card>
-                <Modal
-                    visible={showWarningModal}
-                    onRequestClose={() => setShowWarningModal(false)}
-                >
-                    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-                        <Text>Please select a winner before completing the event.</Text>
-                        <Button onPress={() => setShowWarningModal(false)}>OK</Button>
-                    </View>
-                </Modal>
-            </SafeAreaView>
-        </ScrollView >
+                  >
+                    <Picker.Item label="Select a Winner" value={null} />
+                    {selectedEvent.participants.map((participant, index) => (
+                      <Picker.Item
+                        key={index.toString()}
+                        label={getUsernameFromId(participant)}
+                        value={participant}
+                      />
+                    ))}
+                  </Picker>
+                )}
+              </View>
+              <View styles={styles.attendeeList}>
+                {selectedEvent.isCompleted === "false" ? (
+                  <>
+                    <Button
+                      style={styles.cardButtons}
+                      title="submit"
+                      onPress={() => handleCompleteEvent()}
+                    >
+                      <Text>Post event results</Text>
+                    </Button>
+                    <Button
+                      style={styles.cardButtons}
+                      title="cancel"
+                      onPress={() => {}}
+                    >
+                      <Text>Cancel event</Text>
+                    </Button>
+                    <Button
+                      style={styles.cardButtons}
+                      title="backToEvents"
+                      onPress={() => {}}
+                    >
+                      <Text>Back to Events</Text>
+                    </Button>
+                  </>
+                ) : (
+                  <Text>This event is already completed</Text>
+                )}
+              </View>
+              ;
+            </Card.Content>
+            <View>
+              <Text>Event Prize:</Text>
+              <MonsterImageSelection
+                collectionId={selectedEvent.prizeCollection_id}
+              />
+            </View>
+          </Card>
+          <Modal
+            visible={showWarningModal}
+            onRequestClose={() => setShowWarningModal(false)}
+          >
+            <View
+              style={{
+                flex: 1,
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              <Text>Please select a winner before completing the event.</Text>
+              <Button
+                style={styles.cardButtons}
+                title="submit"
+                onPress={() => setShowWarningModal(false)}
+              >
+                <Text>Post event results</Text>
+              </Button>
+              <Button
+                style={styles.cardButtons}
+                title="cancel"
+                onPress={() => {}}
+              >
+                <Text>Cancel event</Text>
+              </Button>
+              <Button
+                style={styles.cardButtons}
+                title="backToEvents"
+                onPress={() => {}}
+              >
+                <Text>Back to Events</Text>
+              </Button>
+            </View>
+          </Modal>
+        </SafeAreaView>
+      </ScrollView>
     );
 };
 
 const styles = StyleSheet.create({
-    eventCard: {},
-    eventView: {
-        flexDirection: "row",
-        justifyContent: "space-between",
-    },
+  eventCard: {},
+  eventView: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  cardButtons: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "center",
+    backgroundColor: "purple",
+    marginBottom: 15,
+    marginTop: 15,
+  },
 });
 
 export default MyEventPage;
