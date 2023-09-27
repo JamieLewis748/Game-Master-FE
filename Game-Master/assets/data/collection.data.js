d7a555c2db3124266c7a1457a228576cc72e6b2e
xi-xo <jose_aguiar@hotmail.co.uk>
2023-09-27 16:57:16 +0100

    adds hardcoded data for collections

---
+++ Game-Master/assets/data/collection.data.js
@@ -0,0 +1,18 @@
+exports.collectionData =
+[
+    {
+        "_id": "00000020f61bb4362eee2c01",
+        "name": "grass",
+    },
+    {
+        "_id": "00000020f61bb4362eee2c02",
+        "name": "water",
+    },
+    // {
+    //     "_id": "00000020f61bb4362eee2c03",
+    //     "name": "fire",
+    // },
+    {
+        "_id": "00000020f61bb4362eee2c04",
+        "name": "wind",
+    }];
