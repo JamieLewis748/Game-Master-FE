b7a1b277389b0a1654eee87e3fd5aeb960bb5d86
liamjones2 <liammjoness@gmail.com>
2023-09-28 09:26:29 +0100

    fixingXP and error

--- Game-Master/components/APIs/completeEvent.js
+++ Game-Master/components/APIs/completeEvent.js
@@ -2,21 +2,21 @@ import axios from "axios";
 
 
 const completeEvent = (event_id, host_id, participants, winner, duration) => { 
-    console.log(eventId, '<== eventId')
+    console.log(event_id, '<== eventId')
     console.log(host_id, "<== hostId");
     console.log(participants, "<== participants");
     console.log(winner, "<== winner");
     console.log(duration, "<== duration");
-        return axios
-          .patch(`https://game-master-be.onrender.com/api/events/${event_id}`, {
-            "host_id": host_id,
-            "participants": participants,
-            "winner": winner,
-            "duration": duration
-          })
-          .then((response) => {
-            console.log(response);
-          });
+        // return axios
+        //   .patch(`https://game-master-be.onrender.com/api/events/${event_id}`, {
+        //     "host_id": host_id,
+        //     "participants": participants,
+        //     "winner": winner,
+        //     "duration": duration
+        //   })
+        //   .then((response) => {
+        //     console.log(response);
+        //   });
 };
 
 export default completeEvent;
--- Game-Master/components/MyEventPage.js
+++ Game-Master/components/MyEventPage.js
@@ -102,6 +102,7 @@ const MyEventPage = ({ route }) => {
       });
   }, []);
 
+
   return (
       <ScrollView>
         <SafeAreaView
