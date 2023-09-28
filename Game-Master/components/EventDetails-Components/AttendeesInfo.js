0c21093f75fce2d788addc5301fb553b6703f79c
BytingMicrochips <marcus.bishop.1995@live.co.uk>
2023-09-28 15:41:11 +0100

    restyled attendeesInfo add spacing

--- Game-Master/components/EventDetails-Components/AttendeesInfo.js
+++ Game-Master/components/EventDetails-Components/AttendeesInfo.js
@@ -9,27 +9,41 @@ const AttendeesInfo = ({ userList, host, participants }) => {
   return (
     <View>
       <View style={{ flexDirection: "row", alignItems: "center" }}>
-        <IconButton icon="account-circle" size={16} color="gray" />
+        <IconButton icon="account-circle" size={16} color="gray" style={{}} />
         <Paragraph style={{ fontWeight: "bold" }}>
           Currently attending:
         </Paragraph>
       </View>
       {users &&
         users.map((user, index) => (
-          <View style={{ flexDirection: "row", alignItems: "center" }} key={index}>
-            <Paragraph>
+          <View
+            style={{ flexDirection: "row", alignItems: "center" }}
+            key={index}
+          >
+            <Paragraph
+              style={{
+                fontWeight: 400,
+                marginTop: 5
+              }}
+            >
               {user.img_url ? (
-              <Image
-                source={{ uri: user.img_url }}
-                style={{ width: 30, height: 30, borderRadius: 15 }}
-              />
-            ) : null}
+                <Image
+                  source={{ uri: user.img_url }}
+                  style={{
+                    width: 30,
+                    height: 30,
+                    borderRadius: 15,
+                    marginRight: 15,
+                    marginBottom: -10
+                  }}
+                />
+              ) : null}
+
               {user.username}
               {user._id === host && " (Host)"}
             </Paragraph>
           </View>
         ))}
-
     </View>
   );
 };
